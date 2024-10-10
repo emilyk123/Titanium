98ebcadab9be6c28487741d6d0d66fb841e0a08a
Ola <oyinkansola_o@yahoo.com>
2024-10-10 09:38:17 -0700

    not done

--- game.py
+++ game.py
@@ -1,6 +1,3 @@
-
-# Used tutorial https://www.youtube.com/watch?v=2gABYM5M0ww&list=LL&index=5&t=2735s&ab_channel=DaFluffyPotato
-# How to create custom events: https://stackoverflow.com/questions/24475718/pygame-custom-event
 import sys
 import pygame
 from player import Player
@@ -8,44 +5,35 @@ from object import MovingRectangle
 
 class Game:
     def __init__(self):
-        pygame.init() 
+        pygame.init()
 
         screen_width = 640
         screen_height = 480
-        # Create game window
         self.screen = pygame.display.set_mode((screen_width, screen_height))
-        # Create clock used to limit frame rate
         self.clock = pygame.time.Clock()
-        # Create custom event
+
         self.player_move_event = pygame.USEREVENT + 1
-        # [Up, Left, Down, Right]
         self.player_movement = [False, False, False, False]
 
         # Create player at position 50, 50
         self.player = Player((50, 50))
 
-        # instance 
-        self.mover = MovingRectangle(x=screen_width, y=200, width=500, height=60, speed=-5) 
+        # instance
+        self.mover = MovingRectangle(x=screen_width, y=200, width=500, height=60, speed=-5)
 
     def run(self):
-        # Every 1 second the player can move
         pygame.time.set_timer(self.player_move_event, 1000)
 
         while True:
-            # Checks all key and mouse presses
             for event in pygame.event.get():
-                # Pressing the red x at the corner or the window closes the game
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-                
-                # Allow player to move
+
                 if event.type == self.player_move_event:
                     self.player.can_move = True
-                
-                # Check for input with WASD or the arrow keys
+
                 if event.type == pygame.KEYDOWN:
-                    # If timer has passed time limit, allow player input
                     if self.player.can_move:
                         if event.key == pygame.K_w or event.key == pygame.K_UP:
                             self.player_movement[0] = True
@@ -55,16 +43,11 @@ class Game:
                             self.player_movement[2] = True
                         if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
                             self.player_movement[3] = True
-                        
-                        # Subtract player_movement[3] (Right) from player_movement[1] (Left) to get horizontal direction
-                        # Subtract player_movement[2] (Down) from player_movement[0] (Up) to get vertical direction
-                        self.player.move((self.player_movement[3] - self.player_movement[1], self.player_movement[2] - self.player_movement[0]))
 
-                        # Don't allow player movement until timer has met time limit
+                        self.player.move((self.player_movement[3] - self.player_movement[1], self.player_movement[2] - self.player_movement[0]))
                         self.player.can_move = False
 
                 if event.type == pygame.KEYUP:
-                    # When keys are released, set player_movement back to false
                     if event.key == pygame.K_w or event.key == pygame.K_UP:
                         self.player_movement[0] = False
                     if event.key == pygame.K_a or event.key == pygame.K_LEFT:
@@ -73,25 +56,22 @@ class Game:
                         self.player_movement[2] = False
                     if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
                         self.player_movement[3] = False
-            
-        
-            # Recolor the background so it covers everything from the last frame
+
             self.screen.fill((0, 0, 0))
-            
-            # moves the rectangle
             self.mover.move()
-    
-            # draws the rectangle and color red
             self.mover.draw(self.screen, "RED")
 
-            # Draw the player at its current location to the screen
+            # Check for collision between the player and the moving rectangle
+            if self.player.rect.colliderect(self.mover.rect):
+                # Check if player is on top of the rectangle (not just colliding from the side)
+                if self.player.rect.bottom <= self.mover.rect.bottom:
+                    # Move the player with the rectangle's speed
+                    self.player.rect.x += self.mover.speed
+                    self.player.position[0] = self.player.rect.x
+
             self.player.render(self.screen)
 
-            # Updates the display to show all changes made to the game
             pygame.display.update()
-            
-            # Makes the game run at 60 frames per second
             self.clock.tick(60)
 
 Game().run()
-
--- player.py
+++ player.py
@@ -1,16 +1,40 @@
-# https://www.pygame.org/docs/ref/time.html#pygame.time.set_timer
+# # https://www.pygame.org/docs/ref/time.html#pygame.time.set_timer
+# import pygame
+# class Player:
+#     def __init__(self, position):
+#         self.can_move = True
+#         self.position = list(position)
+    
+#     def move(self, movement):
+#         if self.can_move:
+#             # position = [xPosition, yPosition]
+#             self.position[0] += movement[0] * 50
+#             self.position[1] += movement[1] * 50
+    
+#     def render(self, surface):
+#         # Draw rectangle to the surface
+#         pygame.draw.rect(surface, pygame.Color(0, 255, 0), pygame.Rect(self.position[0], self.position[1], 50, 50))
+
+
 import pygame
+
 class Player:
     def __init__(self, position):
         self.can_move = True
         self.position = list(position)
-    
+        # Create a rect for the player object
+        self.rect = pygame.Rect(self.position[0], self.position[1], 50, 50)  # 50x50 is the size of the player
+
     def move(self, movement):
         if self.can_move:
-            # position = [xPosition, yPosition]
+            # Update position
             self.position[0] += movement[0] * 50
             self.position[1] += movement[1] * 50
-    
+            # Update the rect's position
+            self.rect.topleft = self.position
+
     def render(self, surface):
-        # Draw rectangle to the surface
-        pygame.draw.rect(surface, pygame.Color(0, 255, 0), pygame.Rect(self.position[0], self.position[1], 50, 50))
+        # Update rect's position before drawing (in case it changed)
+        self.rect.topleft = self.position
+        # Draw the player using the rect
+        pygame.draw.rect(surface, pygame.Color(0, 255, 0), self.rect)
