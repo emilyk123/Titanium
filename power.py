3eab1306cd9221551d2521b8efcd0ccb5b908909
Emily Krohn <emilykrohn2@gmail.com>
2024-10-22 23:09:43 -0700

    Update power up width and height

--- power.py
+++ power.py
@@ -8,8 +8,8 @@ class PowerUp:
 
         # Initialize color and size
         self.color = (255, 0, 0)
-        self.width = 60
-        self.height = 60
+        self.width = 16
+        self.height = 16
 
         # Set initial random position
         self.x = random.randint(0, self.screen_width - self.width) #***********
